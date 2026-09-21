71592bd08da0ee3d9ed80f4b8661d8dd07adf4a7
simonliu1118-byte <286269326+simonliu1118-byte@users.noreply.github.com>
2026-09-21 16:16:30 +0800

    chore(CYInvoice): require cloud schema 4

--- apps/CYInvoice/src/CYInvoice.Core/CloudCompatibility.cs
+++ apps/CYInvoice/src/CYInvoice.Core/CloudCompatibility.cs
@@ -4,7 +4,7 @@ public static class CloudCompatibility
 {
     public const string ServiceName = "cyinvoice-cloud";
     public const string ApiVersion = "1";
-    public const string SchemaVersion = "3";
+    public const string SchemaVersion = "4";
 
     public static string Problem(CloudHealthResult health)
     {
